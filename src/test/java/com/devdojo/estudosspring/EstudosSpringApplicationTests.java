872ec5625bfe621f9eea8c33e13ece1f16cd30ac
Marvin Mendes <marvin.m.lopes@gmail.com>
2021-05-07 10:40:33 -0300

    Criando o projeto

---
+++ src/test/java/com/devdojo/estudosspring/EstudosSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devdojo.estudosspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudosSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
